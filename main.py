36fea267ef6853f62d63f04240af8110e04f4061
JonathanTTSouza <68395424+JonathanTTSouza@users.noreply.github.com>
2021-07-05 10:04:29 -0300

    exception handling

--- main.py
+++ main.py
@@ -1,25 +1,3 @@
-# student_dict = {
-#     "student": ["Angela", "James", "Lily"],
-#     "score": [56, 76, 98]
-# }
-#
-# #Looping through dictionaries:
-# for (key, value) in student_dict.items():
-#     #Access key and value
-#     pass
-#
-# import pandas
-# student_data_frame = pandas.DataFrame(student_dict)
-#
-# #Loop through rows of a data frame
-# for (index, row) in student_data_frame.iterrows():
-#     #Access index and row
-#     #Access row.student or row.score
-#     pass
-#
-# # Keyword Method with iterrows()
-# # {new_key:new_value for (index, row) in df.iterrows()}
-
 #TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
 import pandas
@@ -28,7 +6,13 @@ nato_csv = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in nato_csv.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("What's the word you want to convert to NATO?: ").upper()
+while True:
+    try:
+        word = input("What's the word you want to convert to NATO?: ").upper()
+        nato_word = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters allowed")
+    else:
+        print(nato_word)
+        break
 
-nato_word = [nato_dict[letter] for letter in word]
-print(nato_word)
